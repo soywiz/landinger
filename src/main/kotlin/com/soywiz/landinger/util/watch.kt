bf459b0e8c0984ada92747c4d2107e95e4a23cfe
soywiz <soywiz@gmail.com>
2020-07-12 20:42:34 +0200

    Reexplore folders on change to support newly created folders

--- src/main/kotlin/com/soywiz/landinger/util/watch.kt
+++ src/main/kotlin/com/soywiz/landinger/util/watch.kt
@@ -5,44 +5,61 @@ import java.io.File
 import java.nio.file.StandardWatchEventKinds
 import java.nio.file.WatchEvent
 import java.nio.file.WatchKey
-import java.util.*
+import kotlin.collections.LinkedHashMap
 
 fun File.watchTree(block: (event: WatchEvent<*>) -> Unit): Cancellable {
     var running = true
-    val file = this.absoluteFile
-    val watchService = file.toPath().fileSystem.newWatchService()
+    val rootFolder = this.absoluteFile
+    val watchService = rootFolder.toPath().fileSystem.newWatchService()
 
-    val keys = arrayListOf<WatchKey>()
+    val keys = LinkedHashMap<File, WatchKey>()
+    val keysRev = LinkedHashMap<WatchKey, File>()
 
-    fun registerFile(file: File) {
+    fun registerFileOnce(file: File) {
         //println("Register $file...")
-        keys += file.toPath().register(
-            watchService,
-            StandardWatchEventKinds.ENTRY_CREATE,
-            StandardWatchEventKinds.ENTRY_DELETE,
-            StandardWatchEventKinds.ENTRY_MODIFY,
-            StandardWatchEventKinds.OVERFLOW
-        )
+        keys.getOrPut(file) {
+            file.toPath().register(
+                watchService,
+                StandardWatchEventKinds.ENTRY_CREATE,
+                StandardWatchEventKinds.ENTRY_DELETE,
+                StandardWatchEventKinds.ENTRY_MODIFY,
+                StandardWatchEventKinds.OVERFLOW
+            ).also {
+                keysRev[it] = file
+            }
+        }
+    }
+
+    fun registerFolders() {
+        for (sfile in rootFolder.walkTopDown()) {
+            if (sfile.isDirectory){
+                registerFileOnce(sfile)
+            }
+        }
     }
 
     Thread(Runnable {
-        println("Watching: $file")
+        println("Watching: $rootFolder")
 
-        for (sfile in file.walkTopDown()) {
-            if (sfile.isDirectory){
-                registerFile(sfile)
-            }
-        }
+        registerFolders()
 
         try {
             while (running) {
                 val watchKey = watchService.take() // This call is blocking until events are present
                 if (!running) break
+
+                // Last queued event
+                if (watchService.poll() == null) {
+                    Thread.sleep(10L)
+                    if (watchService.poll() == null) {
+                        registerFolders()
+                    }
+                }
                 for (event in watchKey.pollEvents()) block(event)
                 if (!watchKey.reset()) break
             }
         } finally {
-            for (key in keys) {
+            for (key in keys.values) {
                 key.cancel()
             }
             watchService.close()
